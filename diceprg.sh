ad690bcc96a70146bf042bed873daf9c3ec84c8d
mandara11 <mandaram2345@gmail.com>
2022-03-10 08:49:28 +0530

    Added:modified

--- diceprg.sh
+++ diceprg.sh
@@ -1,2 +1,24 @@
+
 echo "Dice program"
+
+echo "Random num: " $random
+random=$(( RANDOM%6 + 1 ))
+if(( $random == 1 ))
+then
+        echo "one"
+elif(( $random == 2 ))
+then
+        echo "Two"
+elif(( $random == 3 ))
+then
+        echo "Three"
+elif(( $random == 4 ))
+then
+        echo "Four"
+elif(( $random == 5 ))
+then
+        echo "Five"
+else
+        echo "six"
+fi
 
