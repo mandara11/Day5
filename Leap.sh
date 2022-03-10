61af3c90e72e93c58c2ab4a7be0fa62529483544
mandara11 <mandaram2345@gmail.com>
2022-03-10 09:47:49 +0530

    Added:modified

--- Leap.sh
+++ Leap.sh
@@ -1,2 +1,9 @@
 echo "Leap year program using ifelse"
 
+read -p "Enter the year: " year
+if(( $year % 400 == 0 || $year % 4 == 0 && $year % 100 != 0 ))
+then
+        echo "Leap year"
+else
+        echo "Not a leap year"
+fi
